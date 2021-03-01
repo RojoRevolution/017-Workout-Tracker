88d86077cc7c7052b853a41162c2b52df7f8154a
David Rojo <rojo@rojorevolution.com>
2021-03-01 16:33:50 -0600

    got html pages rendering via controller.js

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+// const path = require('path');
 
-
-
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
 
-// routes
-app.use(require("./routes/api.js"));
+// Import routes and give the server access to them.
+const routes = require('./controllers/appController');
+app.use(routes);
+
+// app.get('/', (req, res) => {
+
+// })
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
